864b65d71dea71d6a836fbc3b808489a4ef825f7
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-03 23:40:57 +0300

    full name

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const monthlyRent = 500;
 const yearlyRent = monthlyRent * 12;
 const decadelyRent = yearlyRent * 10;
 
-const myName = `Olivia Murimi`;
 
-console.log(myName);
+const firstName ="Olivia";
+const lastName "Murimi";
+
+const sentence ="Hello" + firstName + "" + lastName + "How are you?!"
+
+console.log(sentence);
