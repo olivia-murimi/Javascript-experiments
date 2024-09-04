f0fd621ec0a6465028f2516f50f20264d70440d9
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-04 23:07:44 +0300

    true

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const monthlyRent = 500;
+const isTheSkyBlue = true;
 
-const yearlyRent = monthlyRent * 12;
-const decadelyRent = yearlyRent * 10;
-
-
-const firstName ="Olivia";
-const lastName "Murimi";
-
-const sentence = `Hello ${firstName} ${lastName}! How are you !?`;
-
-console.log(sentence);
+console.log(isTheSkyBlue);
