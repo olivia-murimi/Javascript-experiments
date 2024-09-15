d765e896ac0cd9e5ce365662eac1d24209741fa3
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-15 22:16:24 +0300

    if staments

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const friendsAtYouParty = 10;
 
 if(friendsAtYouParty === 0){
   console.log("Cool. now i have all the nachos to myself"); 
+}else if(friendsAtYouParty <= 4){
+  console.log("Perfect amount to play some Mario Kart");
+}else {
+  console.log("Woooooo.play the dance music")
 }
