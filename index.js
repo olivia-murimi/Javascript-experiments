9ca3505438bd3e2e73dd2cc04380f9108d331dd4
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-16 22:19:04 +0300

    loops

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const friendsAtYouParty = 10;
+let friendsAtYourParty = 0;
 
-if(friendsAtYouParty === 0){
-  console.log("Cool. now i have all the nachos to myself"); 
-}else if(friendsAtYouParty <= 4){
-  console.log("Perfect amount to play some Mario Kart");
-}else {
-  console.log("Woooooo.play the dance music")
-}
+friendsAtYourParty = friendsAtYourParty +1;
+
+console.log
