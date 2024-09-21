eed5ed0982731b57743b9f3425451a25db32c6d0
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-21 22:51:50 +0300

    functions

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function addTwo(number) {
 
 const finalAnswer = addTwo(5);
 const finalAnswer2 =addTwo(10);
+const finalAnswer3 =add(5,10);
 console.log(finalAnswer);
-console.log(finalAnswer2);.
+console.log(finalAnswer2);
+console,log(finalAnswer3);
 
 function add(num1,num2){
   return num1 + num2;
