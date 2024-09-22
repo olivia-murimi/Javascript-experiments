e5eb801dd387ae27672d19f8c9d8b98cad884b90
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-22 23:03:08 +0300

    id

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log('Hello');
-console.log('I like Pizza');
+//console.log('Hello');
+//console.log('I like Pizza');
 
-window.alert(`This is an alert`);
-window.alert(`I like pizza`);
+//window.alert(`This is an alert`);
+//window.alert(`I like pizza`);
+document.getElementById("myh1").textContent = 'Hello';
+//This is a comment
