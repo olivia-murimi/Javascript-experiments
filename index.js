5fc6a32e07439f17b95087509146745b4ea1aaf5
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-22 22:52:10 +0300

    javascript

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-function greet(firstName, lastName, honorific, greeting){
-  return `${greeting} ${honorific} ${lastName}!I'm extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
-}
-
-console.log(greet("Olivia","Murimi","Lady","Salutations"));
+console.log('Hello');
+console.log('I like Pizza');
