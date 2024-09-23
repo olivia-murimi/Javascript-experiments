57d0ee0e0e438744f3f844df6c3c6c46b78cd617
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-23 23:33:42 +0300

    variables

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ document.getElementById("myh1").textContent = 'Hello';
 document.getElementById("my p").textContent = 'I like pizza';
 //This is a comment
 let x;
+x = 100;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(gpa);
