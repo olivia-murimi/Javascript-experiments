1bdbebabdac5c6c56292fe533655e0b25993061b
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-23 23:40:48 +0300

    typeof

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ x = 100;
 
 console.log(x);
 
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+//let age = 25;
+//let price = 10.99;
+//let gpa = 2.1;
 
-console.log(`You are ${age} years old`);
-console.log(`The price is ${price}`);
-console.log(`Your gpa is${gpa}`);
+let firstName = "Olivia";
+
+console.log(typeof firstName);
+
+//console.log(`You are ${age} years old`);
+//console.log(`The price is ${price}`);
