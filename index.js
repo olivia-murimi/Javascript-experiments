b9ed8d81060cda689d1c0c4e869429c5d8714040
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-25 23:47:14 +0300

    html

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ username = window.prompt("what's your username");
 console.log(username);
 let username;
 
-document.getElementById("mysubmit").onclick = function(){
- username = document.getElementById("mytext").value;
+document.getElementById("mySubmit").onclick = function(){
+  let username = document.getElementById("myText").value;
+ document.getElementById("myH1").textContent = `Hello ${username}`;
  console.log(username);
 }
