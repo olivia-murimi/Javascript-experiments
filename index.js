0a08050392a49c4fe62d72eebbcfb9e3a8cc8c93
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-27 23:38:25 +0300

    variables

--- index.js
+++ index.js
@@ -73,13 +73,17 @@
 //age = Number(age);
 //age+=1;
 //console.log(age, typeof age);
-let x = "pizza";
-let y = "pizza";
-let z = "pizza";
+//let x = "pizza";
+//let y = "pizza";
+//let z = "pizza";
 
-x = Number(x);
-y = String(y);
-z = Boolean(z);
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+//x = Number(x);
+//y = String(y);
+//z = Boolean(z);
+//console.log(x, typeof x);
+//console.log(y, typeof y);
+//console.log(z, typeof z);
+
+let pi = 3.14159;
+let radius;
+let circumference
