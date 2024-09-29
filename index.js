8d7d639adaaa05bcb006f2551be0023af9d97acc
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-30 00:09:19 +0300

    javascript

--- index.js
+++ index.js
@@ -97,14 +97,17 @@ document.getElementById("mySubmit").onclick = function(){
 Math.PI
 
 console .log(Math.PI);
-let x =81;
+let x =-3.21;
 let y =2;
 let z;
 
 //z=Math.round(x);
 //z = Math.floor(x);
 //z = Math.cell(x);
-z = Math.trunc(x);
-z = Math.pow(x,y);
-z = Math.sqrt(x)
+//z = Math.trunc(x);
+//z = Math.pow(x,y);
+//z = Math.sqrt(x)
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.abs(x);
 console.log(z);
